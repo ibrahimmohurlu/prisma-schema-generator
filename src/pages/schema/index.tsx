42ada6bab7ea64d32985be6dd2f3f2d0f91a46ca
ibrahimmohurlu <ibrahimmohurlu@gmail.com>
2023-04-02 00:25:11 +0300

    initial commit

---
+++ src/pages/schema/index.tsx
@@ -0,0 +1,27 @@
+
+
+
+const SchemaPage = () => {
+
+
+
+    return (
+        <div className="flex justify-center">
+            <div className="absolute top-3 ">
+                <div className="flex flex-row gap-8">
+                    <button type="button" className="bg-gray-200 p-2">Add table</button>
+                    <button type="button" className="bg-gray-200 p-2">generate</button>
+                </div>
+
+            </div>
+            <main className="w-screen h-screen bg-red-100">
+
+
+            </main>
+        </div>
+    )
+}
+
+
+export default SchemaPage;
+
